1120d5b91d7b88ae2b1a4d4bed1197b4ba4ca46f
Bijay Kadariya <bijaykadariya@bijays-mbp.myfiosgateway.com>
2023-05-07 13:57:43 -0400

    Question no2 : answer

--- solution.js
+++ solution.js
@@ -18,3 +18,19 @@ console.log(setAlarm(false, true));
 console.log(setAlarm(true, false));
 */
 //-------------------------------------------------------//
+
+//Write a function named oddNumberCount which receives one argument that should be a number. The function should return how many positive odd numbers there are below the number passed into the argument.
+/*
+function oddNumberCount(number) {
+  let count = 0;
+  for (let i = 0; i < number; i += 1) {
+    if (i % 2 !== 0) {
+      count = count + 1;
+    }
+  }
+  return count;
+}
+console.log(oddNumberCount(7));
+*/
+
+//-----------------------------------------------------//
